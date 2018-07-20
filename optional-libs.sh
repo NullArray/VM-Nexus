d1f61c173639aa525d206ece7a69a6e4b8918fdc
NullArray <NullArray@users.noreply.github.com>
2018-07-20 23:26:01 +0000

    Update optional-libs.sh

--- optional-libs.sh
+++ optional-libs.sh
@@ -5,31 +5,31 @@ function pkgs(){
 	
 
 	
-	sudo apt-get install    \
-		libaio-dev 			\
+	sudo apt-get install            \
+		libaio-dev 		\
 		libbluetooth-dev	\
 		libbrlapi-dev 		\
-		libbz2-dev			\
-		libcap-dev 			\
+		libbz2-dev		\
+		libcap-dev 		\
 		libcap-ng-dev 		\
-		libcurl4-gnutls-dev \
+		libcurl4-gnutls-dev 	\
 		libgtk-3-dev		\
 		libibverbs-dev 		\
 		libjpeg8-dev 		\
 		libncurses5-dev 	\
-		libnuma-dev			\
-		librbd-dev			\
+		libnuma-dev		\
+		librbd-dev		\
 		librdmacm-dev		\
 		libsasl2-dev 		\
 		libsdl1.2-dev 		\
 		libseccomp-dev 		\
 		libsnappy-dev 		\
 		libssh2-1-dev		\
-		libvde-dev 			\
+		libvde-dev 		\
 		libvdeplug-dev 		\
-		libxen-dev 			\
+		libxen-dev 		\
 		liblzo2-dev 		\
-		xfslibs-dev			\
+		xfslibs-dev		\
 		valgrind
 		
 	echo -e "\nRunning updates...\n" && sleep 3
