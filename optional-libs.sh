b7874fa7003218dbdcf69bf31dfdd7ff590b1ba6
NullArray <NullArray@users.noreply.github.com>
2018-07-20 23:25:11 +0000

    Create optional-libs.sh

---
+++ optional-libs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# QEMU optional supporting libs
+function pkgs(){
+	
+
+	
+	sudo apt-get install    \
+		libaio-dev 			\
+		libbluetooth-dev	\
+		libbrlapi-dev 		\
+		libbz2-dev			\
+		libcap-dev 			\
+		libcap-ng-dev 		\
+		libcurl4-gnutls-dev \
+		libgtk-3-dev		\
+		libibverbs-dev 		\
+		libjpeg8-dev 		\
+		libncurses5-dev 	\
+		libnuma-dev			\
+		librbd-dev			\
+		librdmacm-dev		\
+		libsasl2-dev 		\
+		libsdl1.2-dev 		\
+		libseccomp-dev 		\
+		libsnappy-dev 		\
+		libssh2-1-dev		\
+		libvde-dev 			\
+		libvdeplug-dev 		\
+		libxen-dev 			\
+		liblzo2-dev 		\
+		xfslibs-dev			\
+		valgrind
+		
+	echo -e "\nRunning updates...\n" && sleep 3
+	sudo apt-get update && sudo apt-get upgrade
+	
+	clear && echo "Done"
+}
+
+if [[ "$EUID" -ne 0 ]]; then
+   echo -e "\nPlease run this script as root\n"
+else
+	read -p "Install optional supporting QEMU libs? [Y/n]: " choice
+	if [[ $choice == 'y' || $choice == 'Y' ]]; then
+		pkgs
+	else
+		echo -e "\nAborted\n"
+	fi
+fi
